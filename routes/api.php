46c5e75a3a5f6f55034fc857c90fe8c3167f93cf
Pouria <pnsadeghy@gmail.com>
2025-03-19 14:38:23 +0330

    Feat: Add Auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
+use \App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')
+    ->name('auth.')
+    ->controller(AuthController::class)->group(function () {
+    Route::post('login', 'login')->name('login');
+    Route::post('register', 'register')->name('register');
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('logout', 'logout')->name('logout');
+    });
+});
+
+Route::prefix('user')
+    ->name('user.')
+    ->middleware(['auth:sanctum'])->group(function () {
+
+});
