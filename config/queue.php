64b7e341d34e3d4b8ec5d15feac0edce9f3e4a58
Pouria <pnsadeghy@gmail.com>
2025-03-19 13:50:58 +0330

    Feat: Add Deploy files

--- config/queue.php
+++ config/queue.php
@@ -72,6 +72,15 @@ return [
             'after_commit' => false,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'host' => env('RABBITMQ_HOST', 'localhost'),
+            'port' => env('RABBITMQ_PORT', 5672),
+            'user' => env('RABBITMQ_USER', 'guest'),
+            'password' => env('RABBITMQ_PASSWORD', 'guest'),
+            'vhost' => env('RABBITMQ_VHOST', '/'),
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+        ],
     ],
 
     /*
