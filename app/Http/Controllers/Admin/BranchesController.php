92c82988e538f64737098627a7b492d326de8af8
Pouria <pnsadeghy@gmail.com>
2025-03-19 17:58:45 +0330

    Feat: Add Admin Branch controller

---
+++ app/Http/Controllers/Admin/BranchesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @group Admin
+ *
+ * @subgroup Branch
+ *
+ * API endpoints for managing branches
+ */
+class BranchesController extends Controller
+{
+    // TODO Complete: BranchesController
+}
