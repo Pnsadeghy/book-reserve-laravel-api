752f8a26650e5effbe198816f3fc5413b772af86
Pouria <pnsadeghy@gmail.com>
2025-03-19 21:01:06 +0330

    Feat: Add ApplyVisibleMiddleware

---
+++ app/Http/Middleware/ApplyVisibleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Book;
+use App\Models\Scopes\VisibleScope;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApplyVisibleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Book::addGlobalScope(VisibleScope::class);
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \App\Http\Middleware\UserRoleMiddleware::class,
+            'visible' => \App\Http\Middleware\ApplyVisibleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
