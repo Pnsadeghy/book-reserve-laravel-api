ae35ad9e373999db281391011f224b3232165247
Pouria <pnsadeghy@gmail.com>
2025-03-19 17:43:13 +0330

    Feat: Add Book factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->unique()->words(1, true),
+            'description' => $this->faker->unique()->words(2, true),
+            'visible' => true,
+        ];
+    }
+
+    public function unvisible(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'visible' => false,
+            ];
+        });
+    }
+}
