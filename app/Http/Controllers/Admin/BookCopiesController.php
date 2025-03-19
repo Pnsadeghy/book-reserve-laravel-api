d17937a776c2b7f7784de5fe311ee0ba1a8238ee
Pouria <pnsadeghy@gmail.com>
2025-03-19 22:03:20 +0330

    Feat: Add BookCopiesController actions

---
+++ app/Http/Controllers/Admin/BookCopiesController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\BookCopy\AdminBookCopyStoreRequest;
+use App\Http\Requests\Admin\BookCopy\AdminBookCopyUpdateRequest;
+use App\Http\Requests\CommonIndexRequest;
+use App\Http\Resources\Admin\Book\AdminBookCopyResource;
+use App\Interfaces\IBookCopyRepository;
+use App\Models\Book;
+use App\Models\BookCopy;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+/**
+ * @group Admin
+ *
+ * @subgroup Book
+ *
+ * @authenticated
+ *
+ * API endpoints for managing book
+ */
+class BookCopiesController extends Controller
+{
+    public function __construct(protected IBookCopyRepository $repository) {}
+
+    /**
+     * All book copies
+     *
+     * @responseFile 200 resources/responses/Admin/BookCopy/index.json
+     */
+    public function index(CommonIndexRequest $request, Book $book): JsonResponse
+    {
+        return response()->json(AdminBookCopyResource::collection($book->copies()->get()));
+    }
+
+    /**
+     * Store new book copy
+     *
+     * @bodyParam title required
+     * @bodyParam status required values: available,under_repair,lost
+     * @bodyParam condition required values: good,worn,damaged
+     * @bodyParam branch_id required
+     *
+     * @responseFile 201 resources/responses/Admin/BookCopy/store.json
+     */
+    public function store(AdminBookCopyStoreRequest $request, Book $book): JsonResponse
+    {
+        $data = $request->validated();
+        $data['book_id'] = $book->id;
+
+        $model = $this->repository->store($data);
+
+        return response()->json(new AdminBookCopyResource($model), 201);
+    }
+
+    /**
+     * Show book copy
+     *
+     * @responseFile 200 resources/responses/Admin/BookCopy/show.json
+     */
+    public function show(BookCopy $bookCopy): JsonResponse
+    {
+        return response()->json(new AdminBookCopyResource($bookCopy), 201);
+    }
+
+    /**
+     * Update book copy
+     *
+     * @bodyParam title
+     * @bodyParam condition values: good,worn,damaged - Only when the copy is not reserved
+     * @bodyParam status values: available,under_repair,lost
+     * @bodyParam branch_id Only when the copy is not reserved
+     *
+     * @response 200
+     */
+    public function update(AdminBookCopyUpdateRequest $request, BookCopy $bookCopy): JsonResponse
+    {
+        $this->repository->update($bookCopy, $request->validated());
+
+        return response()->json([]);
+    }
+
+    /**
+     * Destroy book copy
+     *
+     * @response 204
+     */
+    public function destroy(BookCopy $bookCopy): Response
+    {
+        $this->repository->delete($bookCopy);
+
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Resources/Admin/BookCopy/AdminBookCopyResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Admin\Book;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdminBookCopyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'branch_id' => $this->branch_id,
+            'title' => $this->title,
+            'visible' => $this->is_visible,
+            'status' => $this->status,
+            'condition' => $this->condition,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
