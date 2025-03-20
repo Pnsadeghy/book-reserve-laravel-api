42428ee53002169a60c27e0c4fa477d2b85616b7
Pouria <pnsadeghy@gmail.com>
2025-03-20 06:58:49 +0330

    Feat: Add BookCopyIndexTest

---
+++ tests/Feature/Admin/BookCopy/BookCopyIndexTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Admin\BookCopy;
+
+use App\Models\Book;
+use App\Models\BookCopy;
+use App\Models\Branch;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookCopyIndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private function getUrl(string $id): string
+    {
+        return '/api/admin/books/'.$id.'/copies';
+    }
+
+    public function test_index(): void
+    {
+        $user = User::factory()->create([
+            'is_admin' => true,
+        ]);
+        $this->actingAs($user);
+
+        $book_1 = Book::factory()->create();
+        $book_2 = Book::factory()->create();
+        $branch = Branch::factory()->create();
+        BookCopy::factory(5)->create([
+            'book_id' => $book_1->id,
+            'branch_id' => $branch->id,
+            'is_visible' => false,
+        ]);
+        BookCopy::factory(5)->create([
+            'book_id' => $book_1->id,
+            'branch_id' => $branch->id,
+            'is_visible' => true,
+        ]);
+        BookCopy::factory(5)->create([
+            'book_id' => $book_2->id,
+            'branch_id' => $branch->id,
+        ]);
+
+        $response = $this->getJson($this->getUrl($book_1->id));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonCount(10);
+
+        $response->assertJsonStructure([
+            '*' => [
+                'id',
+                'title',
+                'visible',
+                'status',
+                'condition',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+}
